a546dd5f428eeed6d907e02a76e9afa2f883c425
Gabryel Deusino <gdeusinosousafreitas@gmail.com>
2025-06-27 00:50:54 -0300

    initial commit

---
+++ src/main/java/br/com/gabyrel/EscolaSpringbootCrudApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabyrel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaSpringbootCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscolaSpringbootCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/gabyrel/EscolaSpringbootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabyrel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaSpringbootCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
